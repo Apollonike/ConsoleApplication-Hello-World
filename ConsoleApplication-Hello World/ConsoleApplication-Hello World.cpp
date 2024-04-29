adc7fc98e8a5f2f407ebdd3e06d4e2d351045d6c
Frank Geier <frank.geier@hotmail.com>
2024-04-29 13:29:49 +0200

    add three more to test

--- ConsoleApplication-Hello World/ConsoleApplication-Hello World.cpp
+++ ConsoleApplication-Hello World/ConsoleApplication-Hello World.cpp
@@ -3,4 +3,7 @@
 int main()
 {
     std::cout << "Hello World 123456789!\n";
+    std::cout << "Hello World 1234567890!\n";
+    std::cout << "Hello World 123456789a!\n";
+    std::cout << "Hello World 123456789b!\n";
 }
